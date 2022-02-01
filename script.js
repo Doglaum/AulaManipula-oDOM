d10eb877c7e39edd49050088d1565e696b17182d
Douglas Martins <90656335+Doglaum@users.noreply.github.com>
2022-02-01 10:05:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var ano = horario.getFullYear()
 var indiceMes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 var mes = horario.getMonth()
 var horas = horario.getHours()
-
+var dia = horario.getDate()
 var minutos = horario.getMinutes()
 var img = document.getElementById("imagem")
 document.getElementById("periodo").innerText = `Agora são exatamente: ${horas} horas e ${minutos} minutos`
@@ -21,7 +21,7 @@ if (horas > 5 && horas < 12) {
 document.getElementById('periodo').addEventListener("mouseover", over);
 document.getElementById('periodo').addEventListener("mouseout", out);
 function over() {
-    document.getElementById('periodo').innerText = `${indiceMes[mes]} de ${ano}`
+    document.getElementById('periodo').innerText = `${dia} de ${indiceMes[mes]} de ${ano}`
 }
 function out() {
     document.getElementById("periodo").innerText = `Agora são exatamente: ${horas} horas e ${minutos} minutos`
